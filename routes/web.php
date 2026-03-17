0c977adf7a41e17836dfa063eedbd78ff554560b
Esther03u <150509740+Esther03u@users.noreply.github.com>
2026-03-17 15:50:10 +0700

    Initial project upload

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MemberController;
+
+Route::get('/member', [MemberController::class, 'index']);
+Route::get('/member/all', [MemberController::class, 'all']);
+Route::get('/member/show/{id}', [MemberController::class, 'show']);
+Route::get('/member/insert', [MemberController::class, 'insert']);
+Route::post('/member/insert2', [MemberController::class, 'insert2']);
+Route::get('/member/list', [MemberController::class, 'member_list']);
+Route::get('/member/update/{id}', [MemberController::class, 'update']);
+Route::post('/member/update2', [MemberController::class, 'update2']);
+Route::get('/member/delete/{id}', [MemberController::class, 'delete']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
